fb4ac7fd7a49d6b71bd194d741b7f3dff978bafb
bougheys <bougheys@JMR387>
2015-12-03 11:36:54 +0000

    This is a test fro using GIT

---
+++ TestJava/src/HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld
+{
+	public static void main(String args[])
+	{
+		//Variables
+		String name = args[0];
+		String name2 = args[1];
+		
+		//This prints output to the screen
+		System.out.println("Hello "+name);
+		System.out.println("Welcome "+name2);
+	}
+
+}
